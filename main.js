608317c011729ab82a563cc27bbbde6811fad783
jonndoe <qb89@yandex.ru>
2020-04-29 01:00:44 +0300

    simple vue example working

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ var app = new Vue({
     data: {
         message: "Hello world",
         value: 5,
+        imgSrc: "https://d2jq2hx2dbkw6t.cloudfront.net/365/vuejs-tutorials.png",
+        link: "https://www.vue.com",
     }
 })
